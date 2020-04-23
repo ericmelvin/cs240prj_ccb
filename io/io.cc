bded67afd860f15866b61b3c754e858aecf1b689
ericmelvin <bshweet@gmail.com>
2020-04-23 17:42:22 -0400

    Added table output functions

--- io/io.cc
+++ io/io.cc
@@ -7,10 +7,20 @@
 
 // Main for testing only
 int main() {
+    // Testing for generate input numbers
     std::vector<int> v = genInputNumbers(100, 100000);
     for (int i=0; i<v.size(); i++) {
         std::cout << v[i] << std::endl;
     }
+
+    // Testing for output table
+    std::vector<std::string> h = {"Data Structure", "Size", "buildTree()", "findMin()", "findMax()", "findK()"};
+
+    std::vector<std::vector<double>> timingData = {{1, 2, 3.1, 4.5, 5},
+                                {5.1235, 7.835, 189.383, 15.5, 1}};
+    std::vector<std::string> rowLabels = {"BST", "RBT"};
+    outputTable(timingData, h, rowLabels);
+
     return 0;
 }
 
@@ -24,3 +34,55 @@ std::vector<int> genInputNumbers(int length, int max) {
     }
     return v;
 }
+
+// Return milliseconds since epoch time
+unsigned long getCurrentTime()  {
+    const auto clock = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+                   clock.time_since_epoch()).count();
+}
+
+// Ouput table functions -------------------------------------------------------
+// Ouput full table
+void outputTable(std::vector<std::vector<double>> timingData, std::vector<std::string> colLabels, std::vector<std::string> rowLabels) {
+    int precision = 10;
+    outputColLabels(colLabels);
+    for (int i=0; i<rowLabels.size(); i++) {
+        outputRowLabel(rowLabels[i]);
+        for (int j=0; j<colLabels.size()-1; j++) {
+            outputData(timingData[i][j], precision);
+        }
+        std::cout << std::endl;
+        outputDivider(colLabels.size()*17);
+    }
+}
+// Ouput column headers
+void outputColLabels(std::vector<std::string> header) {
+    for (int i=0; i<header.size(); i++) {
+        std::cout << std::left <<"| " << std::setw(15) << header[i];
+    }
+    std::cout << std::endl;
+    outputDivider(header.size()*17);
+
+}
+
+// Ouput divider line
+void outputDivider(int size) {
+    for (int i=0; i<size; i++) {
+        std::cout << '-';
+    }
+    std::cout << std::endl;
+}
+
+// Ouput row label
+void outputRowLabel(std::string label) {
+    std::cout << std::left << "| " << std::setw(15) << label;
+}
+
+// Output data set to precision p
+void outputData(double data, int p) {
+    std:: cout << std::left <<"| " << std::setw(15) << std::setprecision(p) 
+    << data;
+}
+
+
